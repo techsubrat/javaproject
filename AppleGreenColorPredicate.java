261f7dd93b90483c936b0920eb5ed570f9b4af66
techsubrat <techsubrat@users.noreply.github.com>
2019-02-28 15:46:44 +0530

    Create AppleGreenColorPredicate.java

---
+++ AppleGreenColorPredicate.java
@@ -0,0 +1,10 @@
+package behavior.parameterization;
+public class AppleGreenColorPredicate implements ApplePredicate {
+
+    @Override
+    public boolean test(Apple apple) {
+        // TODO Auto-generated method stub
+        return "green".equals(apple.getColor());
+    }
+
+}
