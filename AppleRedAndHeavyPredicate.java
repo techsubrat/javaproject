821df549d9bbfe429d3fcdfdeb8917f470cb0a31
techsubrat <techsubrat@users.noreply.github.com>
2019-02-28 15:46:10 +0530

    Create AppleRedAndHeavyPredicate.java

---
+++ AppleRedAndHeavyPredicate.java
@@ -0,0 +1,7 @@
+package behavior.parameterization;
+
+public class AppleRedAndHeavyPredicate implements ApplePredicate {
+    public boolean test(Apple apple) {
+        return "red".equals(apple.getColor()) && apple.getWeight() > 150;
+    }
+}
